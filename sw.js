4d9f3e4b96342819755c36167e8851c4effbc04c
Will Mendes <willmendesneto@gmail.com>
2017-05-03 23:25:21 +1000

    perf: improving user experience for web mobile users

---
+++ sw.js
@@ -0,0 +1,32 @@
+var version = '1.0.0';
+
+self.addEventListener('install', function(e) {
+  var timeStamp = Date.now();
+  e.waitUntil(
+    caches.open('willmendesneto.github.io').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html?timestamp=' + timeStamp,
+        '/assets/css/normalize.min.css?timestamp=' + timeStamp,
+        '/assets/css/foundation.min.css?timestamp=' + timeStamp,
+        '/assets/css/index.css?timestamp=' + timeStamp,
+        '/assets/images/profile.png?timestamp=' + timeStamp
+      ])
+      .then(function() {
+        return self.skipWaiting();
+      });
+    })
+  )
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
