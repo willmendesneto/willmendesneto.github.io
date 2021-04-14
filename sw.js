ed4b25b5f21f008127f15fd592fd2876ed4b087d
Will Mendes <willmendesneto@gmail.com>
2021-04-14 13:15:08 -0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-04c1d198788d77656462.js"
+    "url": "webpack-runtime-5839ef7e8327e2163379.js"
   },
   {
     "url": "styles.5732ca1b73dd34ab0a97.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bc8db2d7434d5c004388a524d567f7c1"
+    "revision": "9d6bb2537dc334b787f1e1bdcd3ab6a7"
   },
   {
     "url": "static/webfonts/s/poppins/v15/pxiByp8kv8JHgFVrLGT9Z1xlFQ.woff2"
