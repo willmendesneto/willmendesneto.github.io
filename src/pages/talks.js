3df2713c1b705b33b294affc8308a2ab1a710ef9
Will Mendes <willmendesneto@gmail.com>
2025-04-28 11:06:14 -0300

    feat: opening talk links in another browser tab

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -10,29 +10,63 @@ const TalksPage = ({ location }) => (
     <>
       <SEO title="Talks" />
       <Title>TALKS</Title>
-      <Link href="https://docs.google.com/presentation/d/1ikvHjY0QBVKraAPVSTaBVoGe86NNRuS4aMiXML1Xd_4/edit?usp=sharing">
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://docs.google.com/presentation/d/1ikvHjY0QBVKraAPVSTaBVoGe86NNRuS4aMiXML1Xd_4/edit?usp=sharing"
+      >
         AI-Powered Angular: Building Smart Angular Apps with Gemini AI & OpenAI
       </Link>
-      <Link href="https://slides.com/willmendesneto/building-large-monorepo-apps-with-nx">
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://slides.com/willmendesneto/building-large-monorepo-apps-with-nx"
+      >
         Building Scalable Monorepos with NX
       </Link>
-      <Link href="https://slides.com/willmendesneto/performance-analytics-and-reliability-in-frontend-apps">
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://slides.com/willmendesneto/performance-analytics-and-reliability-in-frontend-apps"
+      >
         Performance, Analytics and Reliability in Frontend Apps
       </Link>
-      <Link href="https://slides.com/willmendesneto/angular-modules-lets-have-ngfun">
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://slides.com/willmendesneto/angular-modules-lets-have-ngfun"
+      >
         Angular modules: Let’s have #ngFun!
       </Link>
-      <Link href="https://slides.com/willmendesneto/user-timing-api-because-perf-matters">
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://slides.com/willmendesneto/user-timing-api-because-perf-matters"
+      >
         User Timing API: because performance matters
       </Link>
-      <Link href="https://slides.com/willmendesneto/micro-frontend-in-angular-apps">
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://slides.com/willmendesneto/micro-frontend-in-angular-apps"
+      >
         Micro Frontend in Angular Apps
       </Link>
-      <Link href="https://slides.com/willmendesneto/data-driven-frontend-components-and-the-new-jira-navigation">
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://slides.com/willmendesneto/data-driven-frontend-components-and-the-new-jira-navigation"
+      >
         Data-Driven Frontend Components and the New Jira Navigation
       </Link>
-      <Link href="https://slides.com/willmendesneto/angular-and-seo">The Progressive S.E.O. guide for PWA’s</Link>
-      <Link href="https://slides.com/willmendesneto/angular-components-decoupled-shared-reusable-and-opensource">
+      <Link target="_blank" rel="noopener noreferrer" href="https://slides.com/willmendesneto/angular-and-seo">
+        The Progressive S.E.O. guide for PWA’s
+      </Link>
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://slides.com/willmendesneto/angular-components-decoupled-shared-reusable-and-opensource"
+      >
         Angular Components: Decoupled, Shared, Reusable & OpenSource
       </Link>
     </>
